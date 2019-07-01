4a326dac014002c432bb0c1052d6ee53cab2804f
lzw <993096281@qq.com>
2019-07-01 03:09:20 -0400

    test

---
+++ test_sh/fedora_novelsm_test.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+
+bench_db_path="/home/hm/ceshi"
+bench_mem_path="/pmem/nvm"
+bench_value="4096"
+write_buffer_size="64"  #单位：MB
+nvm_buffer_size="8192"  #单位：MB
+
+#bench_benchmarks="fillseq,stats,readseq,readrandom,stats" #"fillrandom,fillseq,readseq,readrandom,stats"
+#bench_benchmarks="fillrandom,stats,readseq,readrandom,stats"
+bench_benchmarks="fillrandom,stats,wait,stats,readseq,readrandom,readrandom,readrandom,stats"
+#bench_benchmarks="fillseq,stats"
+bench_num="2000000"
+bench_readnum="100000"
+
+
+const_params="
+    --db_disk=$bench_db_path \
+    --value_size=$bench_value \
+    --benchmarks=$bench_benchmarks \
+    --num_levels=2 \
+    --num=$bench_num \
+    --reads=$bench_readnum \
+    --db_mem=$bench_mem_path \
+    --write_buffer_size=$write_buffer_size \
+    --nvm_buffer_size=$nvm_buffer_size \
+    "
+
+bench_file_path="$(dirname $PWD )/out-static/db_bench"
+
+if [ ! -f "${bench_file_path}" ];then
+bench_file_path="$PWD/out-static/db_bench"
+fi
+
+if [ ! -f "${bench_file_path}" ];then
+echo "Error:${bench_file_path} or $(dirname $PWD )/out-static/db_bench not find!"
+exit 1
+fi
+
+cmd="$bench_file_path $const_params "
+
+if [ -n "$1" ];then
+cmd="nohup $bench_file_path $const_params >>out.out 2>&1 &"
+echo $cmd >out.out
+fi
+
+echo $cmd
+eval $cmd
--- test_sh/novelsm_test_4value.sh
+++ test_sh/novelsm_test_4value.sh
@@ -5,54 +5,54 @@ test_all_size=81920000000   #80G
 
 
 bench_db_path="/mnt/ssd/ceshi"
+bench_mem_path="/pmem/nvm"
 bench_value="4096"
-bench_compression="none" #"snappy,none"
+write_buffer_size="64"  #单位：MB
+nvm_buffer_size="8192"  #单位：MB
 
 #bench_benchmarks="fillseq,stats,readseq,readrandom,stats" #"fillrandom,fillseq,readseq,readrandom,stats"
-#bench_benchmarks="fillrandom,stats,readseq,readrandom,readrandom,readrandom,stats"
+#bench_benchmarks="fillrandom,stats,readseq,readrandom,stats"
 bench_benchmarks="fillrandom,stats,wait,stats,readseq,readrandom,readrandom,readrandom,stats"
 #bench_benchmarks="fillseq,stats"
 bench_num="20000000"
 bench_readnum="1000000"
-#bench_max_open_files="1000"
-max_background_jobs="2"
-#max_bytes_for_level_base="`expr 8 \* 1024 \* 1024 \* 1024`" 
-max_bytes_for_level_base="`expr 256 \* 1024 \* 1024`" 
 
 
-bench_file_path="$(dirname $PWD )/db_bench"
+bench_file_path="$(dirname $PWD )/out-static/db_bench"
 
 bench_file_dir="$(dirname $PWD )"
 
 if [ ! -f "${bench_file_path}" ];then
-bench_file_path="$PWD/db_bench"
+bench_file_path="$PWD/out-static/db_bench"
 bench_file_dir="$PWD"
 fi
 
 if [ ! -f "${bench_file_path}" ];then
-echo "Error:${bench_file_path} or $(dirname $PWD )/db_bench not find!"
+echo "Error:${bench_file_path} or $(dirname $PWD )/out-static/db_bench not find!"
 exit 1
 fi
 
 RUN_ONE_TEST() {
     const_params="
-    --db=$bench_db_path \
+    --db_disk=$bench_db_path \
     --value_size=$bench_value \
     --benchmarks=$bench_benchmarks \
+    --num_levels=2 \
     --num=$bench_num \
     --reads=$bench_readnum \
-    --compression_type=$bench_compression \
-    --max_background_jobs=$max_background_jobs \
-    --max_bytes_for_level_base=$max_bytes_for_level_base \
+    --db_mem=$bench_mem_path \
+    --write_buffer_size=$write_buffer_size \
+    --nvm_buffer_size=$nvm_buffer_size \
     "
     cmd="$bench_file_path $const_params >>out.out 2>&1"
     echo $cmd >out.out
     echo $cmd
-    eval $cmd
+    #eval $cmd
 }
 
 CLEAN_CACHE() {
     rm -f $bench_db_path/*
+    rm -f $bench_mem_path/*
     sleep 2
     sync
     echo 3 > /proc/sys/vm/drop_caches
@@ -67,8 +67,6 @@ COPY_OUT_FILE(){
     \cp -f $bench_file_dir/OP_DATA $res_dir/
     \cp -f $bench_file_dir/OP_TIME.csv $res_dir/
     \cp -f $bench_file_dir/out.out $res_dir/
-    \cp -f $bench_db_path/OPTIONS-* $res_dir/
-    \cp -f $bench_db_path/LOG $res_dir/
 }
 RUN_ALL_TEST() {
     for value in ${value_array[@]}; do
